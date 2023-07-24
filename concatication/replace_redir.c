8c4c4d8af2bec130e54488d82611cc7c4cdab0a5
Rida Ennaciri <rennacir@e3r7p18.1337.ma>
2023-07-24 15:40:17 +0100

    23-07-2023

--- concatication/replace_redir.c
+++ concatication/replace_redir.c
@@ -6,52 +6,64 @@
 /*   By: rennacir <rennacir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 17:38:08 by rennacir          #+#    #+#             */
-/*   Updated: 2023/07/16 13:52:56 by rennacir         ###   ########.fr       */
+/*   Updated: 2023/07/24 15:21:59 by rennacir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+void	replace_redir_help(t_list **tmp, t_list **new_list)
+{
+	int	type;
+
+	type = (*tmp)->type;
+	(*tmp) = (*tmp)->next;
+	if ((*tmp) && (*tmp)->type == WHITE_SPACE)
+		(*tmp) = (*tmp)->next;
+	if ((*tmp))
+	{
+		ft_lstadd_back(new_list, ft_lstnew_tokens(ft_strdup((*tmp)->content),
+				type));
+		(*tmp) = (*tmp)->next;
+	}
+}
+
+void	replace_redir_help_1(t_list **tmp, t_list **new_list, t_env *envir)
+{
+	char	*str;
+	int		type;
+
+	type = (*tmp)->type;
+	if ((*tmp))
+		(*tmp) = (*tmp)->next;
+	if ((*tmp) && (*tmp)->type == WHITE_SPACE)
+		(*tmp) = (*tmp)->next;
+	str = here_doc_case((*tmp)->content, envir);
+	ft_lstadd_back(new_list, ft_lstnew_tokens(str, type));
+	if ((*tmp))
+		(*tmp) = (*tmp)->next;
+}
+
 t_list	*replace_redir(t_list *list, t_env *envir)
 {
-	t_list *tmp;
-	int type;
-	t_list *new_list;
+	t_list	*tmp;
+	t_list	*new_list;
+
 	new_list = NULL;
 	tmp = list;
-	type = 0;
-	char	*str;
 	while (tmp)
 	{
-		if (tmp->type == ARED_OUT || tmp->type == RED_IN || tmp->type == RED_OUT)
-		{
-			type = tmp->type;
-			tmp = tmp->next;
-			if (tmp && tmp->type == WHITE_SPACE)
-				tmp = tmp->next;
-			if (tmp)
-			{
-				ft_lstadd_back(&new_list, ft_lstnew_tokens(ft_strdup(tmp->content), type));
-				tmp = tmp->next;
-			}
-		}
+		if (tmp->type == ARED_OUT || tmp->type == RED_IN
+			|| tmp->type == RED_OUT)
+			replace_redir_help(&tmp, &new_list);
 		else if (tmp->type == HER_DOC)
-		{
-			type = tmp->type;
-			if (tmp)
-				tmp = tmp->next;
-			if (tmp && tmp->type == WHITE_SPACE)
-				tmp = tmp->next;
-			str = here_doc_case(tmp->content, envir);
-			ft_lstadd_back(&new_list, ft_lstnew_tokens(str, type));
-			if (tmp)
-				tmp = tmp->next;
-		}
+			replace_redir_help_1(&tmp, &new_list, envir);
 		else
 		{
-			ft_lstadd_back(&new_list, ft_lstnew_tokens(ft_strdup(tmp->content), tmp->type));
+			ft_lstadd_back(&new_list, ft_lstnew_tokens(ft_strdup(tmp->content),
+					tmp->type));
 			tmp = tmp->next;
 		}
 	}
-	return new_list;
+	return (new_list);
 }
