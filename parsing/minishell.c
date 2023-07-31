3853471d75efec56fb0cff7acd6d5676eb390a53
Halim Labouiti <hlabouit@e3r8p16.1337.ma>
2023-07-31 15:59:45 +0100

    multi commands exec in the making

--- parsing/minishell.c
+++ parsing/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: rennacir <rennacir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 16:24:15 by rennacir          #+#    #+#             */
-/*   Updated: 2023/07/31 12:55:35 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/07/29 14:49:56 by rennacir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -180,31 +180,15 @@ int main(int argc, char **argv, char **env)
 			finalist = final_list(newlist);
 			lastlist = resume(finalist);
 			// tmplast = lastlist;
-			// while (tmplast)
-			// {
-			// 	tmp1 = tmplast->red;
-			// 	printf("rediredctions\n");
-			// 	while (tmp1)
-			// 	{
-			// 		printf("[%s] ---- %d", tmp1->content, tmp1->type);
-			// 		tmp1 = tmp1->next;
-			// 	}
-			// 	printf("\n");
-			// 	printf("double string\n");
-			// 	print_str(tmplast->cmd);
-			// 	printf("\n-----------------------------\n");
-			// 	tmplast = tmplast->next;
-			// }
 			exit_status(lastlist);
 			commands(lastlist, &envir);
-			execution (lastlist);
-			// free_any_stack(list);
-			// free_any_stack(elist);
-			// free_any_stack(flist);
-			// free_any_stack(clist);
-			// free_any_stack(newlist);
-			// free_any_stack_global(finalist);
-			// free_any_stack_final(lastlist);
+			free_any_stack(list);
+			free_any_stack(elist);
+			free_any_stack(flist);
+			free_any_stack(clist);
+			free_any_stack(newlist);
+			free_any_stack_global(finalist);
+			free_any_stack_final(lastlist);
 			free(str);
 		}
 		else
